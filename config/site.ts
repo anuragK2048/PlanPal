69dfb8693402c8445964805a80e272f7a9de656d
anuragK2048 <developeranurag111@gmail.com>
2024-06-20 18:23:11 +0530

    Initial shift commit

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "PlanPal",
+  url: "",
+  description:
+    "This is a web app replicating Notion, featuring real-time collaboration and adjustable workspaces, built with shadcn/ui, NextJS, TAilwindCSS",
+
+  author: {
+    name: "Anurag Kodle",
+    url: "",
+    email: "anuragkodle111@gmail.com",
+  },
+
+  links: {
+    github: "https://github.com/anuragK2048",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
